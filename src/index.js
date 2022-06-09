ede30782129e561c8e132dfe7ed80bed1968bef0
Ashton Tobar <+>
2022-06-09 13:23:40 -0500

    commit tree 373d5d90d9855354fc3b90841047b7edce66c0a9

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // target the input value
+    // *One Way*
+    // console.log(e.target.children[1].value)
+
+    // *Second Way*
+    const input = document.querySelector("input#searchByID");
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
